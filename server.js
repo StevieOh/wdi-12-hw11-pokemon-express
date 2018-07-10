a636e275c09e31505fd14a311a34044c140164b3
Stephanie Obeytek <sobeytek@gmail.com>
2018-07-10 17:15:49 -0500

    index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const Pokemon = require("./models/pokemon")
 
+app.get("/pokemon", (req, res) => {
+  res.render("index.ejs",{
+    pokemon: Pokemon
+  })
+})
 
 app.get("/pokemon", (req, res) => {
   res.send(Pokemon)
